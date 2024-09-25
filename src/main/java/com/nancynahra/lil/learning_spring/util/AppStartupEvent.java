13689f7811f6996aced859361f5617f8ab94869b
Nancy Nahra <nancy.t.nahra@gmail.com>
2024-09-25 13:10:03 -0500

    Initial commit. Embedded a data base with Spring boot

---
+++ src/main/java/com/nancynahra/lil/learning_spring/util/AppStartupEvent.java
@@ -0,0 +1,23 @@
+package com.nancynahra.lil.learning_spring.util;
+
+import com.nancynahra.lil.learning_spring.data.Room;
+import com.nancynahra.lil.learning_spring.data.RoomRepository;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppStartupEvent implements ApplicationListener<ApplicationReadyEvent> {
+
+    private final RoomRepository roomRepository;
+
+    public AppStartupEvent(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        Iterable<Room> rooms = this.roomRepository.findAll();
+        rooms.forEach(System.out::println);
+    }
+}
