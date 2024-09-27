318db12eba860711c728f6ff9ebe66bf7941236f
nancyanahra <nancy.t.nahra@gmail.com>
2024-09-27 13:53:16 -0500

    Built a new web page to show guest information in a table. Built a controller, view, model and service injection.

---
+++ src/main/java/com/nancynahra/lil/learning_spring/web/GuestInfoController.java
@@ -0,0 +1,38 @@
+package com.nancynahra.lil.learning_spring.web;
+
+import com.nancynahra.lil.learning_spring.business.ReservationService;
+import com.nancynahra.lil.learning_spring.data.Guest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/guests")
+public class GuestInfoController {
+
+    private final ReservationService reservationService;
+
+    public GuestInfoController(ReservationService reservationService) {
+
+        this.reservationService = reservationService;
+    }
+
+    @RequestMapping(method= RequestMethod.GET)
+    public String getGuests(Model model) {
+
+        //made method in Reservation Service to make list of guests
+      // List<Guest> guestInfo = this.reservationService.getGuests();
+        model.addAttribute("guests", this.reservationService.getGuests());
+
+        return "hotel-guests";
+
+
+    }
+
+
+
+}
